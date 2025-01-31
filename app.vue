957291f8977155e86f6ac87f129c40055c767b6e
kas <designer.tmn72@gmail.com>
2025-01-31 18:56:09 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtLoadingIndicator :throttle="10" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data, pending, error } = useFetch('http://127.0.0.1:8000/')
+</script>
+
+<template>
+  <div>
+    <h1 v-if="pending">Загрузка...</h1>
+    <h1 v-else-if="error">Ошибка загрузки</h1>
+    <h1 v-else>{{ data.message }}</h1>
+  </div>
+</template>
